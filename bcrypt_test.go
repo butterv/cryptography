7007c33c9afac35debff92e31c209a558c70ff16
istsh <github3104xniiapm@gmail.com>
2019-09-04 01:05:23 +0900

    Add test

--- bcrypt_test.go
+++ bcrypt_test.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"reflect"
 	"testing"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Test_BCrypt_HashPassword(t *testing.T) {
@@ -35,6 +37,68 @@ func Test_BCrypt_HashPassword(t *testing.T) {
 			isCorrect, _ := b.IsCorrectPassword(got, tt.in)
 			if !isCorrect && tt.wantErr == nil {
 				t.Errorf("IsCorrectPassword(%s, %s)=false, _; want true", got, tt.in)
+			}
+		})
+	}
+}
+
+func Test_BCrypt_IsCorrectPassword(t *testing.T) {
+	tests := []struct {
+		name string
+		in   struct {
+			hashedPassword string
+			password       string
+		}
+		want    bool
+		wantErr error
+	}{
+		{
+			name: "regular case is correct",
+			in: struct {
+				hashedPassword string
+				password       string
+			}{
+				"$2a$10$QKlsqjrE8fjsK9yuVAFzreXt.N3WfoGM1yKX5HgFwgAUcPd06MouK",
+				"uh4r5CVsd9TXfmPs",
+			},
+			want:    true,
+			wantErr: nil,
+		},
+		{
+			name: "hashed password too short",
+			in: struct {
+				hashedPassword string
+				password       string
+			}{
+				"hashedPassword",
+				"password",
+			},
+			want:    false,
+			wantErr: bcrypt.ErrHashTooShort,
+		},
+		{
+			name: "mismatched hash and password",
+			in: struct {
+				hashedPassword string
+				password       string
+			}{
+				"$2a$10$QKlsqjrE8fjsK9yuVAFzreXt.N3WfoGM1yKX5HgFwgAUcPd06MouK",
+				"uh4r5CVsd9TXfmPa",
+			},
+			want:    false,
+			wantErr: bcrypt.ErrMismatchedHashAndPassword,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := BCrypt()
+			got, err := b.IsCorrectPassword(tt.in.hashedPassword, tt.in.password)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("IsCorrectPassword(%s, %s)=_, %#v; want %#v", tt.in.hashedPassword, tt.in.password, err, tt.wantErr)
+			}
+			if got != tt.want {
+				t.Errorf("IsCorrectPassword(%s, %s)=%v, _; want %v", tt.in.hashedPassword, tt.in.password, got, tt.want)
 			}
 		})
 	}

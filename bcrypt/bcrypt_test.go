e67df7e9b176a6991ccdc47688ddad0704128f0e
istsh <github3104xniiapm@gmail.com>
2019-09-19 01:10:35 +0900

    Add test

--- bcrypt/bcrypt_test.go
+++ bcrypt/bcrypt_test.go
@@ -77,7 +77,7 @@ func Test_BCrypt_GenerateFromPassword(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GenerateHash(tt.in.password, tt.in.cost)
 			if !reflect.DeepEqual(err, tt.wantErr) {
-				t.Fatalf("GenerateFromPassword(%s, %d)=_, %#v; want %#v", tt.in.password, tt.in.cost, err, tt.wantErr)
+				t.Fatalf("GenerateHash(%s, %d)=_, %#v; want %#v", tt.in.password, tt.in.cost, err, tt.wantErr)
 			}
 			if err != nil {
 				return
@@ -86,13 +86,89 @@ func Test_BCrypt_GenerateFromPassword(t *testing.T) {
 			strs := strings.Split(got, "$")
 			// str[0] is empty
 			if strs[1][0] != majorVersion {
-				t.Errorf("GenerateFromPassword(%s, %d) incorrect major version: %b; want %b", tt.in.password, tt.in.cost, strs[1][0], majorVersion)
+				t.Errorf("GenerateHash(%s, %d) incorrect major version: %b; want %b", tt.in.password, tt.in.cost, strs[1][0], majorVersion)
 			}
 			if strs[1][1] != minorVersion {
-				t.Errorf("GenerateFromPassword(%s, %d) incorrect minor version: %b; want %b", tt.in.password, tt.in.cost, strs[1][1], minorVersion)
+				t.Errorf("GenerateHash(%s, %d) incorrect minor version: %b; want %b", tt.in.password, tt.in.cost, strs[1][1], minorVersion)
 			}
 			if cost := fmt.Sprintf("%02d", tt.in.cost); strs[2] != cost {
-				t.Errorf("GenerateFromPassword(%s, %d) incorrect cost: %s; want %s", tt.in.password, tt.in.cost, strs[1], cost)
+				t.Errorf("GenerateHash(%s, %d) incorrect cost: %s; want %s", tt.in.password, tt.in.cost, strs[1], cost)
+			}
+		})
+	}
+}
+
+func Test_BCrypt_IsCorrectPassword(t *testing.T) {
+	tests := []struct {
+		name string
+		in   struct {
+			passwordHash string
+			password     string
+		}
+		wantErr error
+	}{
+		{
+			name: "regular case",
+			in: struct {
+				passwordHash string
+				password     string
+			}{
+				"$2a$04$Bza.rhCKUi3KPYLP/3xWyuhjeQI6ZQMc3duEg2NyWvG0RwjllK.Me",
+				"11111111",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "regular case",
+			in: struct {
+				passwordHash string
+				password     string
+			}{
+				"$2a$11$.uxjK2m8ZJHvj5sSSOt7.u8cFC5IDo7N1fRg2qbETdZmlCn2OFvui",
+				"ce249uf21FCerv8u4WEFVDc8uc782fcwEC",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "password hash too short",
+			in: struct {
+				passwordHash string
+				password     string
+			}{
+				"$2a$11$.uxjK2m8ZJHvj5sSSOt7.u8cFC5IDo7N1fRg2qbETdZmlCn2OFv",
+				"",
+			},
+			wantErr: ErrHashTooShort,
+		},
+		{
+			name: "password is empty",
+			in: struct {
+				passwordHash string
+				password     string
+			}{
+				"$2a$11$.uxjK2m8ZJHvj5sSSOt7.u8cFC5IDo7N1fRg2qbETdZmlCn2OFvui",
+				"ce249uf21FCerv8u4WEFVDc8uc782fcwEC",
+			},
+			wantErr: ErrPasswordIsEmpty,
+		},
+		{
+			name: "mismatched password hash and password",
+			in: struct {
+				passwordHash string
+				password     string
+			}{
+				"$2a$11$.uxjK2m8ZJHvj5sSSOt7.u8cFC5IDo7N1fRg2qbETdZmlCn2OFvui",
+				"11111111",
+			},
+			wantErr: ErrMismatchedHashAndPassword,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := IsCorrectPassword(tt.in.passwordHash, tt.in.password)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Fatalf("IsCorrectPassword(%s, %s)=_, %#v; want %#v", tt.in.passwordHash, tt.in.password, err, tt.wantErr)
 			}
 		})
 	}
